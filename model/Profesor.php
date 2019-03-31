30f1cf6987c3381867be652f37d83bf9b86dc484
Mayte <may.eater@gmail.com>
2019-03-31 14:49:09 +0200

    Añadido esquema Profesor.php

---
+++ model/Profesor.php
@@ -0,0 +1,36 @@
+Auth::validate()
+<?php
+
+class Profesor {
+
+  var $id;
+  var $nombre;
+  var $apellidos;
+  var $email;
+
+  function __construct($id = null){
+    $profesor = new Profesor();
+
+  }
+
+  function store(){
+
+  }
+
+  function update(){
+
+  }
+
+  function remove() {
+
+  }
+
+  static function get() {
+
+  }
+
+  static function getSingle() {
+
+  }
+
+}
