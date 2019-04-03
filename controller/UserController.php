e5365620d7833dfa8bee5a5935a6e465eb7987ce
irigarae <manuel.blanco@dw1e.online>
2019-04-03 22:41:12 +0200

    Añade UserController.php

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,25 +1,48 @@
 <?php
 
-class User implements iController {
+class UserController implements iController {
 
-    public static function createPage() {
-        
-    }
+  public static function listPage() {
+    $users = User::get();
+    View::userListPage($users);
+  }
 
-    public static function deletePage() {
-        
+  public static function editPage() {
+    // no tengo claro si aquí hay que editar directamente las propiedades o dentro de la página
+    if ($_POST['id']) {
+      $user = new User($_POST['id']);
+      $user->pass($_POST['pass']);
+      $user->mail($_POST['mail']);
+      View::userEditPage($user);
+    } else if ($_GET['id']) {
+      $user = new User($_GET['id']);
+      $user->pass($_GET['pass']);
+      $user->mail($_GET['mail']);
+      View::userEditPage($user);
+    } else {
+      header('Location: /user/');
     }
+  }
 
-    public static function editPage() {
-        
+  public static function createPage() {
+    if ($_POST) {
+      $user = new User();
+      $user->id($_POST['id']);
+      $user->pass(password_hash($_POST['pass'], PASSWORD_DEFAULT)); // cuándo password_hash()?
+      $user->mail($_POST['mail']);
+      $user->store();
+      header('Location: /user/');
+    } else {
+      View::userCreatePage();
     }
+  }
 
-    public static function getSingle() {
-        
+  public static function deletePage() {
+    if ($_GET['id']) {
+      $user = new User($_GET['id']);
+      $user->remove();
     }
-
-    public static function listPage() {
-        
-    }
+    header('Location: /user/');
+  }
 
 }
