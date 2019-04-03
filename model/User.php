4ab29d63f691be7aad5307e4c69060652c7f01a3
Alejandro <rs.alejandro.93@gmail.com>
2019-04-03 16:16:42 +0200

    Movida la clase User.php a la carpeta de modelo. Rehechos los métodos de la clase.

--- model/User.php
+++ model/User.php
@@ -1,25 +1,81 @@
 <?php
 
-class User {
+class User
+{
+    // Propiedades de la tabla
+    private $id;
+    private $pass;
+    private $mail;
+    
+    // Getters y setters
+    public function id($id=null){
+        if($id)
+            $this->id = $id;
+        return $this->id;
+    }
+    public function pass($pass=null){
+        if($pass)
+            $this->pass = $pass;
+        return $this->pass;
+    }
+    public function mail($mail=null){
+        if($mail)
+            $this->mail = $mail;
+        return $this->mail;
+    }
+    
+    // Constructor
+    public function __construct($id=null){
+        if ($id){
+            $user = Database::getUser($id);
+            $user = $user->fetch_assoc();
+            self::id($user['id']);
+            self::pass($user['pass']);
+            self::mail($user['mail']);
+        }
+    }
 
-  public static function create() {
-
-  }
-
-  public static function get() {
-
-  }
-
-  public static function getSingle() {
-
-  }
-
-  public static function edit() {
-
-  }
-
-  public static function remove() {
-
-  }
+    public static function get(){
+        $users_db = Database::getUserList();
+        $users_arr = [];
+        while ($user_db = $users_db->fetch_assoc()) {
+            $user = new User();
+            $user->id($user_db['id']);
+            $user->pass($user_db['pass']);
+            $user->mail($user_db['mail']);
+            $users_arr[] = $user;
+        }
+        return $users_arr;
+    }
+    
+    public function getSingle(){
+        $user_db = Database::getUser($this->id);
+        $user_db = $user_db->fetch_assoc();
+        $user = new User();
+        $user->id($user_db['id']);
+        $user->pass($user_db['pass']);
+        $user->mail($user_db['mail']);
+        return $user;
+    }
+    
+    public function store(){
+        if (!$this->id){
+            $params = [ 'pass' = $this->pass,
+                        'mail' = $this->mail,];
+            Database::insertUser($params);
+            return $this->id = Database::insertId();
+        }
+        return false;
+    }
+    
+    public function update(){
+        $params = [ 'pass' = $this->pass,
+                    'mail' = $this->mail,];
+        return Database::updateUser($this->id,$params);
+    }
+    
+    public function remove(){
+        return Database::deleteUser($this->id);
+    }
 
 }
