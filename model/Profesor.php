23e6bb57cee10f99082eef301c5b735472a82963
Mayte <may.eater@gmail.com>
2019-04-10 12:21:07 +0200

    Redefinición de los metodos de la clase Profesor.php

--- model/Profesor.php
+++ model/Profesor.php
@@ -1,38 +1,85 @@
-Auth::validate()
 <?php
 
 class Profesor {
 
-  $id;
-  $nombre;
-  $apellidos;
-  $email;
+	private $id;
+	private $nombre;
+	private $apellidos;
+  private $email;
 
-  public function __construct($id = null){
-    $profesor = new Profesor();
+	public function __construct( $id = null ){
+		if( $id ){
+			$profesor = Database::getProfesor( $id );
+			$profesor = $profesor->fetch_assoc();
+			$this->id = $profesor['id'];
+			$this->nombre = $profesor['nombre'];
+			$this->apellidos = $profesor['apellidos'];
+      $this->email = $profesor['email'];
+		}
+	}
 
-  }
+	public function id( $id = null){
+		if( $id )
+			$this->id = $id;
+		return $this->id;
+	}
 
-  public function store(){
-    // INSERT
-  }
+	public function nombre( $nombre = null ){
+		if( $nombre )
+			$this->nombre = $nombre;
+		return $this->nombre;
+	}
 
-  public function update(){
-    // UPDATE
-  }
+	public function apellidos( $apellidos = null ){
+		if( $apellidos )
+			$this->apellidos = $apellidos;
+		return $this->apellidos;
+	}
 
-  public function remove() {
-    // DELETE
-  }
+  public function email( $email = null ){
+		if( $email )
+			$this->email = $email;
+		return $this->email;
+	}
 
-  public static function get() {
-    // SELECT ALL
-  }
+	public function store(){
+		if( !$this->id ){
+			$params = [
+				'nombre' => $this->nombre,
+				'apellidos' => $this->apellidos,
+        'email' => $this->email,
+			];
+			Database::insertProfesor( $params );
+			return $this->id = Database::insertId();
+		}
+		return false;
+	}
 
-  public static function getSingle() {
-    // SELECT ONE
-  }
+	public function update(){
+		$params = [
+			'nombre' => $this->nombre,
+			'apellidos' => $this->apellidos,
+      'email' => $this->email,
+		];
+		return Database::updateProfesor( $this->id, $params );
+	}
+
+	public function remove(){
+		return Database::deleteProfesor( $this->id );
+	}
+
+	public static function get(){
+		$profesor_db = Database::getProfesorList();
+		$profesor_arr = [];
+		while( $profesor_db = $profesor_db->fetch_assoc() ){
+			$profesor = new Profesor();
+			$profesor->id( $profesor_db['id'] );
+			$profesor->nombre( $profesor_db['nombre'] );
+			$profesor->apellidos( $profesor_db['apellidos'] );
+      $profesor->email( $profesor_db['email'] );
+			$profesor_arr[] = $profesor;
+		}
+		return $profesor_arr;
+	}
 
 }
-
-?>
