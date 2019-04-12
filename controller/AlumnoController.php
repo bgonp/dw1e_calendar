df980dd6d767f4a3b01a74cc3c9d2f162156a497
Geroboham <geroal.xander@gmail.com>
2019-04-12 14:20:50 +0200

    alumno controller

--- controller/AlumnoController.php
+++ controller/AlumnoController.php
@@ -6,19 +6,51 @@ class AlumnoController implements iController {
 
     
     public static function createPage() {
-        
+        if(!assert($_POST['nombre'])){
+            View::alumnoCreatePage();
+        }
+        else{
+            $alumno = new alumno();
+
+            $alumno->nombre($_POST['nombre']);
+            $alumno->apellido($_POST['apellido']);
+            $alumno->mail($_POST['mail']);
+            
+            $alumno->store();
+
+            header("location: /alumno");
+        }
     }
 
     public static function deletePage() {
-        
+        if(isset($_GET['id'])){
+            $alumno = new alumno($_GET['id']);
+            $alumno->remove();
+        }
+        header("location: /alumno");
     }
 
     public static function editPage() {
-        
+        if(isset($_POST['id'])){
+            $alumno = new alumno($_POST['id']);
+
+            $alumno->nombre($_POST['nombre']);
+            $alumno->apellido($_POST['apellido']);
+            $alumno->email($_POST['email']);
+
+        }
+        elseif ($_GET['id']) {
+            $alumno = new alumno($_GET['id']);
+        }
+        else{
+            header("location: /alumno");
+        }
+
+        View::alumnoEditPage($alumno);
     }
 
     public static function listPage() {
-        
+        View::alumnoListPage(Alumno::get());
     }
 
 }
